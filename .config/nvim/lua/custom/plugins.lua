53e643c1093bc9a263bb102c33adde73466866a9
ToastedToast <hey@toasted.dev>
2024-04-07 18:43:06 +0800

    feat(nvim): add codeium

--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -123,6 +123,17 @@ local plugins = {
   },
   { "nvim-tree/nvim-tree.lua", enabled = false },
   { 'wakatime/vim-wakatime', lazy = false },
-  { 'andweeb/presence.nvim', lazy = false }
+  { 'andweeb/presence.nvim', lazy = false },
+  {
+    'Exafunction/codeium.vim',
+    lazy = false,
+    config = function ()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set('i', '<Tab>', function () return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true, silent = true })
+      vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+    end
+  }
 }
 return plugins
