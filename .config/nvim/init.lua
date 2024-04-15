ab88e0c8684cf831859c852dea07f579a78cd365
ToastedToast <hey@toasted.dev>
2024-04-15 19:46:26 +0800

    fix(neovide): disable animations (again)

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -20,3 +20,9 @@ dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
 
+if vim.g.neovide then
+  vim.g.neovide_scroll_animation_length = 0
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_cursor_trail_size = 0
+end
