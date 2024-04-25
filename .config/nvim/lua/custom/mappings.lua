d5a09ad247396d8ff904cb631a468cafee907f34
ToastedToast <hey@toasted.dev>
2024-04-25 22:16:40 +0800

    fix(nvim): reset close buffer keybind

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -1,23 +1,9 @@
 local M = {}
 
 -- In order to disable a default keymap, use
-M.disabled = {
-  n = {
-    ["<leader>x"] = ""
-  }
-}
+M.disabled = {}
 
 -- Your custom mappings
-M.toasted = {
-  n = {
-     ["<C-w>"] = {
-      function()
-        require("nvchad.tabufline").close_buffer()
-      end,
-      "Close buffer",
-    },
-    -- ...
-  }
-}
+M.toasted = {}
 
 return M
